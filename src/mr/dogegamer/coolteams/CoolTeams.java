3489b63e9642c84833e58e13dc93c37015960497
DogeGamerMC <dmarimon2@gmail.com>
2022-01-04 22:49:59 -0500

    Plugin

---
+++ src/mr/dogegamer/coolteams/CoolTeams.java
@@ -0,0 +1,13 @@
+package mr.dogegamer.coolteams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CoolTeams extends JavaPlugin{
+
+	public static final String prefix = "[CoolTeams] ";
+	
+	@Override
+	public void onEnable() {
+		System.out.println(prefix + "hola");
+	}
+}
