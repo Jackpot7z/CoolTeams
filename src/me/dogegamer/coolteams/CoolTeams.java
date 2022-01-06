e880a10f61fd7fdc724776711fc0eb94459d54ae
DogeGamerMC <dmarimon2@gmail.com>
2022-01-05 23:13:52 -0500

    Added subcommands

--- src/me/dogegamer/coolteams/CoolTeams.java
+++ src/me/dogegamer/coolteams/CoolTeams.java
@@ -1,5 +1,8 @@
 package me.dogegamer.coolteams;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.dogegamer.coolteams.commands.TeamsCommand;
@@ -8,14 +11,28 @@ public class CoolTeams extends JavaPlugin{
 
 	public static final String prefix = "[CoolTeams] ";
 	
+	public static YamlConfiguration config;
+	
+	public static CoolTeams plugin;
+	
 	@Override
 	public void onEnable() {
+		plugin = this;
 		registerCommands();
+		loadConfig();
 		System.out.println(prefix + "hola");
+		
 	}
-
+	
 	private void registerCommands() {
 		this.getCommand("teams").setExecutor(new TeamsCommand());
 	}
 	
+	public void loadConfig() {
+		File file = new File(getDataFolder(), "config.yml");
+		if(!file.exists())
+			saveResource("config.yml", true);
+		config = YamlConfiguration.loadConfiguration(file);
+	}
+
 }
--- src/me/dogegamer/coolteams/commands/TeamsCommand.java
+++ src/me/dogegamer/coolteams/commands/TeamsCommand.java
@@ -4,21 +4,45 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import me.dogegamer.coolteams.CoolTeams;
 import me.dogegamer.coolteams.Messager;
 
 public class TeamsCommand implements CommandExecutor {
 
+	String[] helpLines = {
+			"&a&lCoolTeams",
+			"",
+			"&a/team help &e- shows this menu",
+			"&a/team create [name] &e- create a team",
+			"&a/team remove &e- remove a team",
+			"&a/team join [player] &e- join a team",
+			"&a/team leave &e- leave a team",
+			"&a/team sethome &e- set team home",
+			"&a/team home &e- teleport to team home",
+			"&a/team list &e- list team players",
+			"&a/team kick [player] &e- kicl a team player",
+			"&a/team config &e- show the config commands",
+			"&a/team promote [player] &e- promote a player",
+	};
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String cmd, String[] args) {
 		if(args.length > 0) {
-			// TODO: add subcommands
-			sender.sendMessage(Messager.color(
+			if(args[0].equalsIgnoreCase("reload")) {
+				CoolTeams.plugin.loadConfig();
+				sender.sendMessage(Messager.color(
+						"&aThe configuration has been reloaded."));
+			} else if (args[0].equalsIgnoreCase("help")) {
+			} else {
+				sender.sendMessage(Messager.color(
 					"&cInvalid arguments! &7Use &b/teams help &7to get help."));
+			}
 		} else {
 			sender.sendMessage(Messager.color(
 					"&eUse &b/teams help &eto see how to use the plugin."));
 		}
-		return false;
+		return true;
+	
 	}
 
 }
